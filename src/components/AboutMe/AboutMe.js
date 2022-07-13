282d071085684ff7ec4df95aa370b7337c0b8308
Oleg Azadov <oazadov@gmail.com>
2022-07-13 20:03:06 +0300

    someEdit

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -11,22 +11,38 @@ function AboutMe() {
         <div className='about-me__info'>
           <p className='about-me__name'>Oleg</p>
           <p className='about-me__about'>Фронтенд-разработчик, 30 лет</p>
-          <p className='about-me__description'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
-и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+          <p className='about-me__description'>
+            Рожденный в СССР, выросший в Санкт-Петербурге, вечно молодой. Учился
+            на инженера связи но кривая жизни увела на заработки. В 2021г начал учится на Web разработчика. В свободное время ем (=
+            Люблю готовить и есть.
+            Ну и, само собой, авто  мото - моя страсть.
+          </p>
 
           <ul className='about-me__links'>
             <li className='about-me__link'>
-              <a className='about-me__item opacityEffect' href='https://www.facebook.com' target={'_blank'}>Facebook</a>
+              <a
+                className='about-me__item opacityEffect'
+                href='https://www.facebook.com'
+                target={'_blank'}
+              >
+                Facebook
+              </a>
             </li>
             <li className='about-me__link'>
-              <a className='about-me__item opacityEffect' href='https://github.com/stswm' target={'_blank'}>Github</a>
+              <a
+                className='about-me__item opacityEffect'
+                href='https://github.com/stswm'
+                target={'_blank'}
+              >
+                Github
+              </a>
             </li>
           </ul>
         </div>
         <div className='about-me__img-shell'></div>
         {/* <img className='about-me__img' src={img} alt='Фотография студента' /> */}
       </div>
-      <Portfolio/>
+      <Portfolio />
     </section>
   );
 }
