0e984589ac5182883182b7a202b3d1fbdbc73944
Oleg Azadov <oazadov@gmail.com>
2022-08-04 19:58:32 +0300

    fix: checkbox status changed

--- src/utils/MovieFilter.js
+++ src/utils/MovieFilter.js
@@ -1,16 +1,13 @@
 function moviesFilter(movies, query, checkboxStatus) {
   let moviesFilter = movies;
   let result;
-console.log("moviesFilter 1",moviesFilter);
 if (checkboxStatus) {
   moviesFilter = moviesFilter.filter((movie) => movie.duration <= 40);
-  console.log("moviesFilter 2",moviesFilter);
 }
 
 result = moviesFilter.filter((movie) => {
   return movie.nameRU.toLowerCase().indexOf(query.toLowerCase()) !== -1;
 })
-console.log("moviesFilter 3",moviesFilter);
   return result;
 }
 
